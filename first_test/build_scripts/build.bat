dcb18a344537d4d19eb903a94568e5294cb3c16a
Josh B <89416369+joshb88@users.noreply.github.com>
2026-03-15 22:39:52 -0500

    bad speller is baed

--- first_test/build_scripts/build.bat
+++ first_test/build_scripts/build.bat
@@ -3,4 +3,4 @@
 REM move to project root (one level up from build_scripts)
 cd /d "%~dp0.."
 
-cl65 -C smfc.cfg -o hello.mc -l hello.list -m hello.mmap hello.asm
+cl65 -C smc.cfg -o hello.mc -l hello.list -m hello.mmap hello.asm
